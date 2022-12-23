90f43cb47dbd3468f9c42b4d0a4069b75af84864
INYONG KIM <ohnyong87@gmail.com>
2022-12-23 14:09:57 +0900

    Merge 2

--- target/classes/config/SQL/REVIEW.sql
+++ target/classes/config/SQL/REVIEW.sql
@@ -6,13 +6,6 @@ create table review(
     score number(1) not null,
     filename varchar2(100) default 'noimage.png',
     wdate date default sysdate,
-<<<<<<< HEAD
-    pnum_fk number(8) references product(pnum)
-);
-
-drop sequence review_seq;
-create sequence review_seq nocache;
-=======
     pnum_fk number(8) references item(pidx)
 );
 
@@ -20,4 +13,3 @@ create sequence review_seq nocache;
 drop sequence review_seq;
 create sequence review_seq nocache;
 
->>>>>>> 5151d986498978559cc11216d4d10399f57876be
