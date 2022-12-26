d7cfafdccd3a4fbfe262ef2f8ef883030ce2a0db
신성욱 <jshin0627j@gmail.com>
2022-12-26 17:16:12 +0900

    commit_221226_7
    notice_board 삽입 1차 작성

--- src/main/java/com/wisetree/test/BoardController_notice.java
+++ src/main/java/com/wisetree/test/BoardController_notice.java
@@ -1,18 +1,18 @@
 package com.wisetree.test;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
+import javax.annotation.Resource;
+import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import com.common.CommonUtil;
+import com.notice_board.model.BoardVO;
+import com.notice_board.service.BoardService;
 
 import lombok.extern.log4j.Log4j;
 
@@ -21,9 +21,31 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class BoardController_notice {
 	
+	@Resource(name="boardServiceImpl")
+	private BoardService boardService;
+	
+	@Inject
+	private CommonUtil util;
+	
 	@GetMapping("/write")
 	public String boardWrite() {
 		return "notice_board/boardWrite";
+	}
+	@PostMapping("write")
+	public String boardInsert(Model m,
+			@ModelAttribute BoardVO board) {
+		
+		//boardService의 insertBoard()호출하기
+		int n=0;
+		String str="",loc="";
+		n=this.boardService.insertBoard(board);
+		str+=(n>0)?"성공":"실패";
+		loc=(n>0)?"list":"javascript:history.back()";
+		
+		
+		
+		return util.addMsgLoc(m, str, loc);
+		
 	}
 	
 	
