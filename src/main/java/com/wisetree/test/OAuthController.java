a3db2931b243815de9a3949ac9c44ecf4e32d9ea
INYONG KIM <ohnyong87@gmail.com>
2022-12-28 14:11:12 +0900

    Commit 5-1
    SNS(KAKAO) 로그인 구현중
    
    1. Kakao Developers 애플리케이션(웹) 추가
    - JavaScript 키 발급 : 개인보관
    - Redirect URI를 등록 : http://localhost:9090/oauth/kakao
    - 카카오 로그인 설정 off -> ON
    
    2. login view인 loginmodal 부분에서
    - REST_API키 및 REDIRECT_URi 를 요청하는 링크(로그인 버튼) 생성
    
    3. OauthController에서
    - http://localhost:9090/oauth/kakao Getmapping으로 code 콜백
    
    4. 에디터 콘솔 창 내 코드가 전달 되는 것 확인.
    
    -----추가내용----
    5. code를 이용하여 access token을 발급받는 OAuthService를 작성
    	- connection 생성
    	- POST로 보낼 Body 작성
    	- 받아온 결과 JSON 파싱 (Gson)
    	위 코드는 조금 변형하면, 다른 API 통신에도 모두 사용이 가능
    
    6. 로그인 작동 시 통신 결과가 200이여야 성공
    	- responseCode : 200
    	- access_token 값이 로그로 출력되면 성공

--- src/main/java/com/wisetree/test/OAuthController.java
+++ src/main/java/com/wisetree/test/OAuthController.java
@@ -1,10 +1,13 @@
 package com.wisetree.test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.user.service.OAuthService;
 
 import lombok.AllArgsConstructor;
 
@@ -13,6 +16,8 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/oauth")
 public class OAuthController {
 
+	@Autowired
+	private OAuthService os;
     /**
      * 카카오 callback
      * [GET] /oauth/kakao/callback
@@ -21,6 +26,9 @@ public class OAuthController {
     @GetMapping("/kakao")
     public void kakaoCallback(@RequestParam String code) {
         System.out.println(code);
+     // 위에서 만든 코드 아래에 코드 추가
+     		String access_Token = os.getAccessToken(code);
+     		System.out.println("###access_Token#### : " + access_Token);
     }
 }
 //설정한 localhost:9090/oauth/kakao로 redirect되었고, Query String으로 code를 받는다.
