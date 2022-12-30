4cad2ac77ede4d76f999f1691fa1cfbfd3e35b94
jijong <jjs3787@gmail.com>
2022-12-30 13:37:34 +0900

    commit 12-30
    다시커밋

--- src/main/java/com/wisetree/test/ReviewRESTController.java
+++ src/main/java/com/wisetree/test/ReviewRESTController.java
@@ -103,10 +103,14 @@ public class ReviewRESTController {
 	@GetMapping(value = "", produces = "application/json")
 	public List<ReviewVO> revList(HttpSession sion) {
 		
-		Integer pidx = (Integer) sion.getAttribute("pidx");
+		int pidx = 6;
+		sion.setAttribute("pidx", pidx);
+		sion.setMaxInactiveInterval(-1);
+		
+		Integer pidx1 = (Integer) sion.getAttribute("pidx");
 		System.out.println("rrrrrrrr");
-		log.info("pidx =>" + pidx);
-		List<ReviewVO> rearr = this.reviewService.listReview(pidx);
+		log.info("pidx =>" + pidx1);
+		List<ReviewVO> rearr = this.reviewService.listReview(pidx1);
 		return rearr;
 	}
 
