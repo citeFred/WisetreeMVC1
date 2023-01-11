55a8e4306e3c129df1a72897e81f6186f344b618
신성욱 <jshin0627j@gmail.com>
2023-01-11 13:08:31 +0900

    commit_shin_230111_1
    공지사항 띄우기 230111_1 작업

--- src/main/java/com/wisetree/test/IndexController.java
+++ src/main/java/com/wisetree/test/IndexController.java
@@ -1,12 +1,20 @@
 package com.wisetree.test;
 
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.notice_board.model.BoardVO;
+import com.notice_board.service.BoardService;
 
 @Controller
 public class IndexController {
 	//뷰 네임을 반환하지 않으면(void) RequestMapping으로 등록한 "/top"==>이 뷰 네임이 된다.
 	
+	@Inject
+	BoardService noticeService;
 	@RequestMapping("/top")
 	public void top() {
 		//"/WEB-INF/views/top.jsp
@@ -23,7 +31,9 @@ public class IndexController {
 	}
 
 	@RequestMapping("/index")
-	public void showIndex() {
+	public void showIndex(Model m) {
+		BoardVO vo=noticeService.selectBoardByIdx(2);
+		m.addAttribute("notice",vo);
 		//"/WEB-INF/views/index.jsp				
 	}
 	
